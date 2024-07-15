94052042eb763497365be892c6f552ee5f974dff
nimrashahid20 <nimrashahid02007@gmail.com>
2024-07-14 23:12:42 -0700

    assigment
    assigment chapter 4 is about legal and illegal variable

---
+++ assigment 4.js
@@ -0,0 +1,42 @@
+
+
+
+//1. Declare 3 variables in one statement.
+let name
+let myName
+let lastName
+
+//2. Declare 5 legal & 5 illegal variable names.
+
+                      //legal variable //
+
+let dateOfBirth
+let store20
+let $nimra
+let mybithday
+let father_name
+
+                    //illegal variable //
+
+let 17
+let #
+let *
+let !
+let ^
+
+// 3. Display this in your browser
+// a) A heading stating “Rules for naming JS variables”
+// b) Variable names can only contain ______, ______,
+// ______ and ______.
+// For example $my_1stVariable
+// c) Variables must begin with a ______, ______ or
+// _____. For example $name, _name or name
+// d) Variable names are case _________
+// e) Variable names should not be JS _________
+
+document.write("Rules For Naming JS")
+document.write(" Variable names can only contain numbers,$ M <br> for example:$my_1stVariable")
+document.write("Variables must begin with a letter,$.For Example:$nmae,_name or name")
+document.write(" Variable names are case sensitive")
+document.write(" Variable names should not be JS keywords")
+
